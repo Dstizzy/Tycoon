646ae9e5b9be625a19b7102faffeb9c8a828713d
Kevin <kevinjoelrobles@gmail.com>
2025-10-07 16:34:03 -0500

    Refactored the buildings' button behavior

--- Assets/Scripts/ButtonsPopUp.cs
+++ Assets/Scripts/ButtonsPopUp.cs
@@ -3,19 +3,13 @@ using TMPro;
 using UnityEngine;
 
 public class ButtonsPopUp : MonoBehaviour {
-    [SerializeField] private TMP_Text buttonText;
-    [SerializeField] private TMP_Text buttonText_2;
-    [SerializeField] private TMP_Text buttonText_3;
+    [SerializeField] public TMP_Text buttonText;
 
-
-    public void SetText(string text_1,string text_2, string text_3) {
-        if (buttonText == null && buttonText_2 == null && buttonText_3) {
+    public void SetText(string text_1) {
+        if (buttonText == null) {
             Debug.LogError("PopUp textComponent is not assigned in the Inspector!");
             return;
         }
         buttonText.text = text_1;
-        buttonText_2.text = text_2;
-        buttonText_3.text = text_3;
     }
-
 }
--- Assets/Scripts/PopUp.cs
+++ Assets/Scripts/PopUp.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PopUp : MonoBehaviour {
     [SerializeField] private TMP_Text textComponent;
 
-
     public void SetText(string value) {
         if (textComponent == null) {
             Debug.LogError("PopUp textComponent is not assigned in the Inspector!");
