f1d527f8df40b0623c38d542e52b89ce16e59f7e
Kevin <kevinjoelrobles@gmail.com>
2025-10-12 19:52:45 -0500

    Working on trade hut panel

---
+++ Assets/Scripts/ItemSprites.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ItemSprites : MonoBehaviour
+{
+    public static ItemSprites itemSprites { get; private set; }
+
+    private void Awake() {
+        if (itemSprites != null && itemSprites != this) {
+            Destroy(gameObject);
+        } else {
+            itemSprites = this;
+        }
+    }
+
+    [Header("Item Sprites")]
+    public Sprite crudeTool;
+}
