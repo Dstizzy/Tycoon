3b02981eff8be2da6b90441eaffc9d9866da1135
zachveldman <z.veldman04@gmail.com>
2025-10-13 21:50:31 -0500

    include tradehutmanager call in popupmanager

--- Assets/Scripts/PopUpManager.cs
+++ Assets/Scripts/PopUpManager.cs
@@ -78,4 +78,9 @@ public class PopUpManager : MonoBehaviour {
             }
         }
     }
+
+    public void OnBuildingButtonClick()
+    {
+        TradeHutManager.Instance.ShowTradePanel();
+    }
 }
