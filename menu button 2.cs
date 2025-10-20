897bf3eb6058b54c2312730f6ba340cddb3ac1e2
ChloeL11 <kuroiilz11@gmail.com>
2025-10-20 08:24:37 -0500

    Commit all updated assets and scripts

---
+++ menu button 2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class menu button : ScriptableObject
+{
+	[MenuItem("Tools/MyTool/Do It in C#")]
+	static void DoIt()
+	{
+		EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+	}
+}
+
