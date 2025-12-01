25546ba9411dcefeeb5e546631183168daa8630a
Dakota Craver <dakota.craver@students.pcci.edu>
2025-11-30 22:29:02 -0600

    Added changes to lab

--- Assets/Scripts/NodeHover.cs
+++ Assets/Scripts/NodeHover.cs
@@ -1,13 +1,20 @@
 /* Libraries                                                                                     */
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 
 public class NodeHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     /* Inspector Variables                                                                       */
     [SerializeField] private GameObject tierInfoPopUp; /* Panel that gets activated upon hover   */
+    [SerializeField] private GameObject tierInfoExpanded; /* Expansion of original info          */
 
+
+    public void Start()
+    {
+        tierInfoPopUp.transform.Find("Button").GetComponent<Button>().onClick.AddListener(() => HandleExpandedInfo());
+    }
     /* Implements interface function for entering the game object with mouse                     */
     void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
     {
@@ -26,10 +33,23 @@ public class NodeHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
         tierInfoPopUp.SetActive(true);
     }
 
+    public void HandleExpandedInfo()
+    {
+        if (tierInfoExpanded.activeSelf)
+        {
+            tierInfoExpanded.gameObject.SetActive(false);
+        }
+        else
+        {
+            tierInfoExpanded.gameObject.SetActive(true);
+        }
+  
+    }
+
     /* Sets the info panel inactive upon exiting this object                                     */
     private void OnPointerExit(PointerEventData EventData)
     {
-        tierInfoPopUp.SetActive(false);
+       // tierInfoPopUp.SetActive(false);
     }
 
     
