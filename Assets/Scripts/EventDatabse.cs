a2a9b6f499824ead2276846e0a0e8dfcb88f6647
zachveldman <z.veldman04@gmail.com>
2026-01-25 16:32:00 -0600

    A few node decisions added; Script for events database

---
+++ Assets/Scripts/EventDatabse.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EventDatabse : MonoBehaviour
+{
+   [Header("Regions")]
+   public List<ExploreEvents> shallowsEvents;
+   public List<ExploreEvents> middleEvents;
+   public List<ExploreEvents> deepEvents;
+
+   public ExploreEvents GetRandomEvent(int region)
+   {
+      List<ExploreEvents> selectedPool = null;
+
+      switch(region)
+      {
+         case 1:
+            selectedPool = shallowsEvents;
+            break;
+         case 2:
+            selectedPool = middleEvents;
+            break;
+         case 3:
+            selectedPool = deepEvents;
+            break;
+      }
+
+      if(selectedPool != null && selectedPool.Count > 0)
+      {
+         int randomIndex = Random.Range(0, selectedPool.Count);
+         return selectedPool[randomIndex];
+      }
+
+      Debug.LogWarning("No events were found for region");
+      return null;
+   }
+}
