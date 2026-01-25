21dc6322da15c36ee6e152316e2dffa0026ff429
zachveldman <z.veldman04@gmail.com>
2026-01-25 15:22:03 -0600

    ExploreEvents script with event template

---
+++ Assets/Scripts/ExploreEvents.cs
@@ -0,0 +1,38 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ExploreEvents", menuName = "Scriptable Objects/ExploreEvents")]
+public class ExploreEvents : ScriptableObject
+{
+   public string description;
+   public EventChoice choiceA;
+   public EventChoice choiceB;
+}
+
+[System.Serializable]
+public struct EventChoice
+{
+   public string buttonText;
+
+   [Header("Guaranteed Result")]
+   public int goldChange;
+   public int oreChange;
+   public int healthChange;
+   public int fuelChange;
+   public int inventoryChange;
+   public int harpoonChange;
+   public int artifactChange;
+   public bool waitTurn;
+
+   [Header("Range Result")]
+   public int minGold;
+   public int maxGold;
+   public int minOre;
+   public int maxOre;
+
+   [Header("Chance Results")]
+   [Range(0, 1)] public float fuelChance;
+   public int fuelGain;
+   [Range(0, 1)] public float damageChance;
+   public int healthDamage;
+}
