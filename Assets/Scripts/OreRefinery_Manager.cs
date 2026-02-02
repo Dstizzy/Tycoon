a303413b9814ddca307c0c2ab8dca2481d4a4a83
ChloeL11 <kuroiilz11@gmail.com>
2026-02-02 15:37:38 -0600

    fix resetting craft amount

--- Assets/Scripts/OreRefinery_Manager.cs
+++ Assets/Scripts/OreRefinery_Manager.cs
@@ -2,67 +2,77 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OreRefinery_Manager : MonoBehaviour { 
+public class OreRefinery_Manager : MonoBehaviour
+{ 
 
-    public static OreRefinery_Manager Instance { get; private set; }
+   public static OreRefinery_Manager Instance { get; private set; }
 
-    const int INFO_BUTTON = 1;
-    const int UPGRADE_BUTTON = 2;
-    const int STARTING_LEVEL = 1;
-    const int ENDING_LEVEL = 5;
+   const int INFO_BUTTON = 1;
+   const int UPGRADE_BUTTON = 2;
+   const int STARTING_LEVEL = 1;
+   const int ENDING_LEVEL = 5;
 
-    [SerializeField] private Transform infoPanel;
-    [SerializeField] private Transform upgradePanel;
-    public TextMeshProUGUI oreRefineryLevelText;
+   [SerializeField] private Transform infoPanel;
+   [SerializeField] private Transform upgradePanel;
+   public TextMeshProUGUI oreRefineryLevelText;
 
-    public int oreLevel = STARTING_LEVEL;
+   public int oreLevel = STARTING_LEVEL;
+   public int CurrentOreProduction { get; private set; }
+   public int NextUpgradeCostInPearls { get; private set; }
 
-    public int CurrentOreProduction { get; private set; }
-    public int NextUpgradeCostInPearls { get; private set; }
-
-    private void Awake() {
-        if(Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
+   private void Awake()
+   {
+      if(Instance != null && Instance != this)
+      {
+         Destroy(gameObject);
+         return;
+      }
+      Instance = this;
         
-        if (infoPanel == null) {
-            Debug.LogError("Info Panel is not assigned in the Inspector!");
-        } else {
-            infoPanel.gameObject.SetActive(false);
-        }
+      if (infoPanel == null)
+      {
+         Debug.LogError("Info Panel is not assigned in the Inspector!");
+      }
+      else
+      {
+         infoPanel.gameObject.SetActive(false);
+      }
 
-        TurnManager.OnTurnEnded += ProduceOres;
+      TurnManager.OnTurnEnded += ProduceOres;
 
-        CalculateRefineryValues();
+      CalculateRefineryValues();
 
-        if (oreRefineryLevelText == null) {
-            Debug.LogError("Ore Refinery Level Text is not assigned");
-        } else {
-            oreRefineryLevelText.text = "Level " + oreLevel.ToString();
-        }
-    }
+      if (oreRefineryLevelText == null)
+      {
+         Debug.LogError("Ore Refinery Level Text is not assigned");
+      }
+      else
+      {
+         oreRefineryLevelText.text = "Level " + oreLevel.ToString();
+      }
+   }
 
-    public void RequestOreRefinoryPanel(int buttonID) {
-        switch (buttonID) {
-            case INFO_BUTTON:
-                ShowInfoPanel();
-                infoPanel.transform.Find("ExitButton").GetComponent<Button>().onClick.AddListener(() => CloseOreRefinoryPanel(INFO_BUTTON));
-                break;
-            case UPGRADE_BUTTON:
-                ShowUpgradePanel();
-                upgradePanel.Find("YesButton").GetComponent<Button>().onClick.AddListener(() => UpgradeOreRefinory());
-                upgradePanel.transform.Find("CancelButton").GetComponent<Button>().onClick.AddListener(() => CloseOreRefinoryPanel(UPGRADE_BUTTON));
-                break;
-            default:
-                Debug.Log("Building Panel: Unknown button ID.");
-                break;
-        }
-    }
+   public void RequestOreRefinoryPanel(int buttonID)
+   {
+      switch (buttonID)
+      {
+         case INFO_BUTTON:
+            ShowInfoPanel();
+            infoPanel.transform.Find("ExitButton").GetComponent<Button>().onClick.AddListener(() => CloseOreRefinoryPanel(INFO_BUTTON));
+            break;
+         case UPGRADE_BUTTON:
+            ShowUpgradePanel();
+            upgradePanel.Find("YesButton").GetComponent<Button>().onClick.AddListener(() => UpgradeOreRefinory());
+            upgradePanel.transform.Find("CancelButton").GetComponent<Button>().onClick.AddListener(() => CloseOreRefinoryPanel(UPGRADE_BUTTON));
+            break;
+         default:
+            Debug.Log("Building Panel: Unknown button ID.");
+            break;
+      }
+   }
 
-    public void CloseOreRefinoryPanel(int buttonID) {
+   public void CloseOreRefinoryPanel(int buttonID)
+   {
         switch (buttonID) 
         {
             case INFO_BUTTON:
