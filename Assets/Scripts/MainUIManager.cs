b3e21da583abb28e1dc229daed230de1b0af92db
Dakota Craver <dakota.craver@students.pcci.edu>
2026-02-08 20:19:19 -0600

    Added victory condition and icon

--- Assets/Scripts/MainUIManager.cs
+++ Assets/Scripts/MainUIManager.cs
@@ -3,20 +3,25 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MainUIManager : MonoBehaviour {
-    public Button MainMenuButton;
-    public Button[] DropdownButtons;
-    public Button InventoryButton;
+public class MainUIManager : MonoBehaviour
+{
+   public Button   MainMenuButton;
+   public Button[] DropdownButtons;
+   public Button   InventoryButton;
+   public Button   victoryButton;
 
-    [SerializeField] private TextMeshProUGUI pearCountText;
-    [SerializeField] private TextMeshProUGUI oreCountText;
-    [SerializeField] private Transform       erroPanel;
+   [SerializeField] private TextMeshProUGUI pearCountText;
+   [SerializeField] private TextMeshProUGUI oreCountText;
+   [SerializeField] private Transform       erroPanel;
+   [SerializeField] private GameObject      victoryPanel;
 
-    private bool isVisible = false;
+   private bool isVisible = false;
 
-    private void Awake() {
+   private void Awake()
+   {
 
-      if (InventoryManager.Instance == null) {
+      if (InventoryManager.Instance == null)
+      {
          // This is primarily for safety, though the script execution order should help.
          // You might need to check your Unity Project Settings -> Script Execution Order 
          // to ensure InventoryManager runs before MainUIManager.
@@ -26,34 +31,95 @@ public class MainUIManager : MonoBehaviour {
       }
 
       // Initially hide dropdown buttons
-      foreach (Button btn in DropdownButtons) {
-            btn.gameObject.SetActive(false);
-        }
-        // Add listener to main menu button
-        MainMenuButton.onClick.AddListener(ToggleMenu);
-        InventoryButton.onClick.AddListener(() => {
-            InventoryManager.Instance.ShowInventoryPanel();
-        });
+      foreach (Button btn in DropdownButtons)
+      {
+         btn.gameObject.SetActive(false);
+      }
+      // Add listener to main menu button
+      MainMenuButton.onClick.AddListener(ToggleMenu);
+      InventoryButton.onClick.AddListener(() => {
+         InventoryManager.Instance.ShowInventoryPanel();
+      });
+      victoryButton.onClick.AddListener(() => {
+         ShowVictoryPanel();
+      });
 
-        ChangePearlCountText(InventoryManager.Instance.pearlCount);
-        ChangeOreCountText(InventoryManager.Instance.oreCount);
-        InventoryManager.Instance.OnOreCountChanged   += ChangeOreCountText;
-        InventoryManager.Instance.OnPearlCountChanged += ChangePearlCountText;
-    }
-    public void ToggleMenu() {
-        isVisible = !isVisible;
-        DropdownButtons[0].gameObject.SetActive(isVisible);
-        DropdownButtons[1].gameObject.SetActive(isVisible);
-    }
+      ChangePearlCountText(InventoryManager.Instance.pearlCount);
+      ChangeOreCountText(InventoryManager.Instance.oreCount);
+      InventoryManager.Instance.OnOreCountChanged += ChangeOreCountText;
+      InventoryManager.Instance.OnPearlCountChanged += ChangePearlCountText;
+   }
+   public void ToggleMenu()
+   {
+      isVisible = !isVisible;
+      DropdownButtons[0].gameObject.SetActive(isVisible);
+      DropdownButtons[1].gameObject.SetActive(isVisible);
+   }
 
-    public void ChangePearlCountText(int newPearlCount) {
-        if (pearCountText != null)
-            pearCountText.text = newPearlCount.ToString();
-    }
-   
-    public void ChangeOreCountText(int newOreCount) {
-        if (oreCountText != null)
+   public void ChangePearlCountText(int newPearlCount)
+   {
+      if (pearCountText != null)
+         pearCountText.text = newPearlCount.ToString();
+   }
+
+   public void ChangeOreCountText(int newOreCount)
+   {
+      if (oreCountText != null)
          oreCountText.text = newOreCount.ToString();
-    }
+   }
 
+   public void ShowVictoryPanel()
+   {
+      if (victoryPanel != null)
+      {
+         victoryPanel.SetActive(true);
+         victoryPanel.transform.Find("ExitButton").GetComponent<Button>().onClick.AddListener(() => {
+            victoryPanel.SetActive(false);
+            victoryButton.onClick.RemoveAllListeners();
+            PopUpManager.Instance.EnablePlayerInput();
+         });
+
+         PopUpManager.Instance.DisablePlayerInput();
+
+         // First Part
+         victoryPanel.transform.Find("SubmarineSkel/HeadButton").GetComponent<Button>().onClick.AddListener(() => ActivateFirstPart());
+
+         // Second Part
+         victoryPanel.transform.Find("SubmarineSkel/BodyButton").GetComponent<Button>().onClick.AddListener(() => ActivateSecondPart());
+
+         // Third Part
+         victoryPanel.transform.Find("SubmarineSkel/TailButton").GetComponent<Button>().onClick.AddListener(() => ActivateThirdPart());
+      }
+   }
+
+   public void ActivateFirstPart()
+   {
+      if(victoryPanel.transform.Find("SubmarineSkel/SubmarineBody").gameObject.activeSelf && victoryPanel.transform.Find("SubmarineSkel/SubmarineTail").gameObject.activeSelf)
+      {
+         victoryPanel.transform.Find("SubmarineFull").gameObject.SetActive(true);
+      }
+      else
+      {
+         if (victoryPanel.transform.Find("SubmarineSkel/SubmarineHead").gameObject.activeSelf)
+            victoryPanel.transform.Find("SubmarineSkel/SubmarineHead").gameObject.SetActive(true);
+         else
+            victoryPanel.transform.Find("SubmarineSkel/SubmarineHead").gameObject.SetActive(false);
+      }
+   }
+
+   public void ActivateSecondPart() 
+   {
+      if(victoryPanel.transform.Find("SubmarineSkel/SubmarineBody").gameObject.activeSelf)
+         victoryPanel.transform.Find("SubmarineSkel/SubmarineBody").gameObject.SetActive(true);
+      else
+         victoryPanel.transform.Find("SubmarineSkel/SubmarineBody").gameObject.SetActive(false);
+   }
+
+   public void ActivateThirdPart() 
+   {
+      if(victoryPanel.transform.Find("SubmarineSkel/SubmarineTail").gameObject.activeSelf)
+         victoryPanel.transform.Find("SubmarineSkel/SubmarineTail").gameObject.SetActive(true);
+      else
+         victoryPanel.transform.Find("SubmarineSkel/SubmarineTail").gameObject.SetActive(false);
+   }
 }
