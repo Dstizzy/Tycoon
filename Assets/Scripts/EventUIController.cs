1c7decfe477fb32793a368ce36ee90b65f71661d
zachveldman <z.veldman04@gmail.com>
2026-02-22 23:43:40 -0600

    documentation

--- Assets/Scripts/EventUIController.cs
+++ Assets/Scripts/EventUIController.cs
@@ -5,12 +5,13 @@ using TMPro;
 public class EventUIController : MonoBehaviour
 {
    [Header("UI References")]
-   public TextMeshProUGUI scenarioText;
-   public TextMeshProUGUI choiceAText;
-   public TextMeshProUGUI choiceBText;
-   public Button buttonA;
-   public Button buttonB;
+   public TextMeshProUGUI scenarioText; // Text block where scenario is displayed
+   public TextMeshProUGUI choiceAText; // Text block inside first choice button
+   public TextMeshProUGUI choiceBText; // Text block inside second choice button
+   public Button buttonA; // The first choice button
+   public Button buttonB; // The second choice button
 
+   // Populates the decision panel's text fields with data from the randomly pulled event
     public void SetEventPanel(ExploreEvents currentEvent)
    {
       scenarioText.text = currentEvent.description;
