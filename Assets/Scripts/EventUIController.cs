26ce82169b612327cbf66e527198c19c7a31d1ec
zachveldman <z.veldman04@gmail.com>
2026-03-01 20:55:12 -0600

    Reapply "Exploration unit"
    This reverts commit b7fc14af228945918281c5ce639b6b2e7f9579cf.

--- Assets/Scripts/EventUIController.cs
+++ Assets/Scripts/EventUIController.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class EventUIController : MonoBehaviour
 {
    [Header("UI References")]
-   public TextMeshProUGUI scenarioText;
-   public TextMeshProUGUI choiceAText;
-   public TextMeshProUGUI choiceBText;
-   public Button buttonA;
-   public Button buttonB;
+   public TextMeshProUGUI scenarioText; // Text block where scenario is displayed
+   public TextMeshProUGUI choiceAText; // Text block inside first choice button
+   public TextMeshProUGUI choiceBText; // Text block inside second choice button
 
+   // Populates the decision panel's text fields with data from the randomly pulled event
     public void SetEventPanel(ExploreEvents currentEvent)
    {
       scenarioText.text = currentEvent.description;
--- Assets/Scripts/LabManager.cs
+++ Assets/Scripts/LabManager.cs
@@ -1,6 +1,6 @@
 /* libraries                                                                                     */
 using System;
-
+using System.ComponentModel;
 using TMPro;
 
 using UnityEngine;
@@ -42,8 +42,8 @@ public class LabManager : MonoBehaviour
    public static bool bodyUnlocked = false;
    public static bool tailUnlocked = false;
 
-
    TradeHutManager tradeHutManager;
+   ShipManager shipManager;
 
    public static LabManager labManager;
 
@@ -293,17 +293,17 @@ public class LabManager : MonoBehaviour
          }
          bodyUnlocked = true;
       }
-      /* Tier 1 missions have succession increased by 25%                                      */
+      /* Ships have health and fuel increased                                                    */
       else if (tabType == explorationTab)
       {
-         Debug.Log("Tier 1 missions increased by 25%");
-         tailUnlocked = true;
+         if(shipManager != null)
+            shipManager.ApplyLabShipBonus();
       }
       else
       {
-         Debug.Log("There is no tab");
+          Debug.Log("There is no tab");
       }
-   }
+    }
 
    public void ImplementTierTwoInnovation(GameObject tabType)
    {
@@ -326,16 +326,15 @@ public class LabManager : MonoBehaviour
             ForgeManager.Instance.UnlockOverclock();
          }
       }
-      /* Permanently increase gold by +15 per turn                                             */
+      /* Unlocks chance to find crafts on explorations                                         */
       else if (tabType == explorationTab)
-      {
-         Debug.Log("Permanently increase gold by +15 per turn");
-      }
-      else
-      {
-         Debug.Log("There is no tab");
-      }
-   }
+         if (shipManager != null)
+            shipManager.UnlockTier2Choices();
+         else
+         {
+            Debug.Log("There is no tab");
+         }
+    }
 
    public void ImplementTierThreeInnovation(GameObject tabType)
    {
@@ -367,16 +366,17 @@ public class LabManager : MonoBehaviour
             ForgeManager.Instance.UnlockReduceCraftingTime();
          }
       }
-      /* Decrease search costs by 50%                                                          */
+      /* Double exploration rewards                                                            */
       else if (tabType == explorationTab)
-      {
-         Debug.Log("Decrease search costs by 50%");
-      }
-      else
-      {
-         Debug.Log("There is no tab");
-      }
-   }
+        {
+         if (shipManager != null)
+            shipManager.ApplyLabRewardBonus();
+        }
+        else
+        {
+            Debug.Log("There is no tab");
+        }
+    }
 
    /* Unlock the next tier node upon buying the previous tier node                              */
    public void UnlockNextNode(GameObject tab, int tier)
