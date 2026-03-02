fbb07d617a09b587d70a85203c5c18b37b8cc163
Kevin <kevinjoelrobles@gmail.com>
2026-03-01 21:16:55 -0600

    updated sell tab UI Added patch kit description Changed forge error panel and text color

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -172,9 +172,9 @@ public class InventoryManager : MonoBehaviour
       else
          CraftWindow.gameObject.SetActive(false);
 
-      pearlCount = 10000;
+      pearlCount = 0;
       crystalCount = MIN_CRYSTAL_COUNT;
-      oreCount = 100;
+      oreCount = 0;
       crudeToolCount = MIN_CRUDE_TOOL_COUNT;
       harpoonCount = MIN_HARPOON_COUNT;
       engineCount = MIN_ENGINE_COUNT;
@@ -475,7 +475,7 @@ public class InventoryManager : MonoBehaviour
             break;
          case PATCH_KIT_TAG:
             craftCount = patchKitCount;
-            craftInfo = "";
+            craftInfo = GetItemDescription(ItemType.PatchKit);
             break;
          case PRESSURE_VALVE_TAG:
             craftCount = pressureValveCount;
