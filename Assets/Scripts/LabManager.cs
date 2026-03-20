2c9e68956b24c858f9c8fe24475bf8980dfd5c7d
Kevin <kevinjoelrobles@gmail.com>
2026-03-20 15:51:26 -0500

    Market shift min and max have been changed to 1.06 and 1.2 Fixed the crafting patch kit bug Fixed bug when unlocking lab tiers for the commerce tab

--- Assets/Scripts/LabManager.cs
+++ Assets/Scripts/LabManager.cs
@@ -250,13 +250,13 @@ public class LabManager : MonoBehaviour
                pearlCost     = T2_COMM_PEARL;
                itemCost      = T2_COMM_PRESSURE_VALVE;
                requiredItem  = "Pressure Valve";
-               useItemMethod = inv.TryUsePressureValve;
+               useItemMethod = ForgeManager.Instance.hasTier2Blueprint ? inv.TryUsePressureValve : null;
                break;
             case TIER_THREE:
                pearlCost     = T3_COMM_PEARL;
                itemCost      = T3_COMM_LENS;
                requiredItem  = "Precision Lens";
-               useItemMethod = inv.TryUsePrecisionLens;
+               useItemMethod = ForgeManager.Instance.hasTier3Blueprint ? inv.TryUsePrecisionLens : null;
                break;
          }
       }
@@ -357,8 +357,11 @@ public class LabManager : MonoBehaviour
       bool isSuccess = false;
       
       if(useItemMethod == null) 
+      {
          Debug.LogError("Use item methods was not found.");
-      else
+         ticker.ShowTicker($"{itemName}s have not been unlocked!", Color.red, TickerSystem.MessageTypes.ResultMessage);
+
+      } else
       {
          // Link to inventory to spend the item
          if((inv.pearlCount >= pearlCost))
@@ -387,8 +390,6 @@ public class LabManager : MonoBehaviour
       if (tabType == commerceTab)
       {
          currentCommerceTier = TIER_ONE;
-         tradeHutManager.marketShiftMin = 1;
-         tradeHutManager.marketShiftMax = 2;
 
          foreach (Transform item in tradeHutManager.SellItems)
             item.Find("NextValue").GetComponent<TextMeshProUGUI>().gameObject.SetActive(true);
@@ -428,12 +429,7 @@ public class LabManager : MonoBehaviour
    {
       /* Grant action to gameple 50 gold for 60% chance to get 250 back                        */
       if (tabType == commerceTab)
-      {
-         TradeHutManager.Instance.marketShiftMin = 3;
-         TradeHutManager.Instance.marketShiftMax = 5;
-
          tradeHutManager.RecycleButton.gameObject.SetActive(true);
-      }
 
       /* Unlock tier 2 item (reinforces component); forge now has 5% chance to produce a       */
       /*    bonus item upon crafting a single item                                             */
@@ -464,9 +460,6 @@ public class LabManager : MonoBehaviour
 
       if (tabType == commerceTab)
       {
-         tradeHutManager.marketShiftMin = 5;
-         tradeHutManager.marketShiftMax = 10;
-
          // Removes the negative world events
          tradeHutManager.isTier3BuffACtive = true;
 
--- Assets/Scripts/TradeHutManager.cs
+++ Assets/Scripts/TradeHutManager.cs
@@ -50,7 +50,7 @@ public class TradeHutManager : MonoBehaviour
 
                rawOreExchange            = 0,
                mercenaryEngineerBuyCount = 0,
-       
+
                crudeToolFluctuation,
                harpoonFluctuation,
                divingBellFluctuation,
@@ -68,8 +68,8 @@ public class TradeHutManager : MonoBehaviour
                worldEvent;
 
    // Public variables
-   public int marketShiftMax = 0,
-              marketShiftMin = 0;
+   public float marketShiftMax = 1.2f,
+                marketShiftMin = 1.06f;
    public int shiftDirection { get; private set; } 
 
 
@@ -907,12 +907,12 @@ public class TradeHutManager : MonoBehaviour
        precisionLensChance = Rng.Next(MARKET_CHANCE_MIN, MARKET_CHANCE_MAX + 1);
        engineChance        = Rng.Next(MARKET_CHANCE_MIN, MARKET_CHANCE_MAX + 1);
    
-       crudeToolFluctuation     = Rng.Next(marketShiftMin, marketShiftMax + 1);
-       harpoonFluctuation       = Rng.Next(marketShiftMin, marketShiftMax + 1);
-       pressureValveFluctuation = Rng.Next(marketShiftMin, marketShiftMax + 1);
-       divingBellFluctuation    = Rng.Next(marketShiftMin, marketShiftMax + 1);
-       precisionLensFluctuation = Rng.Next(marketShiftMin, marketShiftMax + 1);
-       engineFluctuation        = Rng.Next(marketShiftMin, marketShiftMax + 1);
+       crudeToolFluctuation     = GetItemSellValueFluctuation(BASE_CRUDE_TOOL_SELL_VALUE);
+       harpoonFluctuation       = GetItemSellValueFluctuation(BASE_HARPON_SELL_VALUE);
+       pressureValveFluctuation = GetItemSellValueFluctuation(BASE_PRESSURE_VALVE_SELL_VALUE);
+       divingBellFluctuation    = GetItemSellValueFluctuation(BASE_DIVING_BELL_SELL_VALUE);
+       precisionLensFluctuation = GetItemSellValueFluctuation(BASE_PRECISION_LENS_SELL_VALUE);
+       engineFluctuation        = GetItemSellValueFluctuation(BASE_ENGINE_VALUE);
    
        // 2. Update UI Previews based on these exact rolls
        UpdatePreviewUI(
@@ -951,6 +951,20 @@ public class TradeHutManager : MonoBehaviour
               MIN_ENGINE_VALUE, MAX_ENGINE_VALUE);
        }
    }
+
+   private int GetItemSellValueFluctuation(int itemBaseValue) 
+   {
+      int fluctuation = itemBaseValue;
+      float currentShift;
+
+      currentShift = (float)Rng.NextDouble() * (marketShiftMax - marketShiftMin) + marketShiftMin;
+
+      Debug.Log("Fluctuation: " + currentShift.ToString());
+
+      fluctuation = (int) Math.Abs(((float)fluctuation - ((float)itemBaseValue * currentShift)));
+
+      return fluctuation;
+   }
    
    // Helper method to keep your UI updates clean and perfectly matched to the math
    private void UpdatePreviewUI(
@@ -971,7 +985,6 @@ public class TradeHutManager : MonoBehaviour
        }
        else
        {
-         // Standard Fluctuation Preview
          if (worldEvent == (int) eventType && TurnManager.Instance.eventCountdown == WORLD_EVENT_ACTIVE_TURN) 
          {
             if(shiftDirection <= 50)
@@ -979,6 +992,7 @@ public class TradeHutManager : MonoBehaviour
             else
               preview  = currentVal + baseValue;
          }
+         // Standard Fluctuation Previewx
          else
          { 
             if (chance <= 30) 
@@ -1294,8 +1308,8 @@ public class TradeHutManager : MonoBehaviour
       TradePanels.gameObject.SetActive(true);
       ShowSellPanel();
 
-      //if (MainUIManager.mainUI != null)
-      //   MainUIManager.mainUI.SetMainButtonsInteractable(false);
+      if (MainUIManager.mainUI != null)
+         MainUIManager.mainUI.SetMainButtonsInteractable(false);
    }
 
    private void ShowInfoPanel() 
