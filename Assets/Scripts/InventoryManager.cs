bf4634a24dc95b7f29a56583291a868d6105fe20
Kevin <kevinjoelrobles@gmail.com>
2026-04-10 09:05:10 -0500

    Changed inventory panel color

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -180,15 +180,15 @@ public class InventoryManager : MonoBehaviour
       else
          CraftWindow.gameObject.SetActive(false);
 
-      pearlCount         = 5000;
-      oreCount           = 5000;
-      crudeToolCount     = 5000;
-      harpoonCount       = 5000;
-      patchKitCount      = 5000;
-      pressureValveCount = 5000;
-      divingBellCount    = 5000;
-      precisionLensCount = 5000;
-      engineCount        = 5000;
+      pearlCount         = 0;
+      oreCount           = 0;
+      crudeToolCount     = 0;
+      harpoonCount       = 0;
+      patchKitCount      = 0;
+      pressureValveCount = 0;
+      divingBellCount    = 0;
+      precisionLensCount = 0;
+      engineCount        = 0;
    }
 
    /* Creates the display elements for Pearls and Crystals on the inventory panel. */
